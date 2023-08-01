4bb05ff52ef73e663f5f5001a2842125d6712e20
Maga Bashirov <magabashirov@Magas-MacBook-Pro-2.local>
2023-08-01 14:37:06 +0300

    module 3 start

---
+++ index.js
@@ -0,0 +1,31 @@
+const yargs = require("yargs");
+const pkg = require("./package.json");
+const { addNote, printNotes } = require("./notes.controller");
+yargs.version(pkg.version);
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true
+    }
+  },
+  handler({ title }) {
+    addNote(title);
+  }
+})
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+   await printNotes();
+  }
+});
+
+
+
+yargs.parse();
